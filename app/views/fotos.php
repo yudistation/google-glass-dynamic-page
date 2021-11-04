7c2217c06e7f5e5314276922cd4e44c396144340
Yudi <gabryudi@gmail.com>
2021-11-03 21:29:47 -0400

    🎨 Improved code structure

--- app/views/fotos.php
+++ app/views/fotos.php
@@ -1,24 +1,24 @@
 <section id="corpo-full">
-  <article id="main-news">
-    <header id="article-header">
-      <hgroup>
-        <h3>Glass > Fotos</h3>
-        <h1>Galeria de Imagens do Google Glass</h1>
-        <h2>por Gustavo Guanabara</h2>
-        <h3 class="right">Atualizado em 01/Maio/2013</h3>
-      </hgroup>
-    </header>
-    <p>
-      Veja na nossa galeria de fotos várias belas imagens que mostram algumas das principais características do Google Glass, como recursos e propriedades que estão impressionando o mundo inteiro. Basta passar o mouse sobre uma das fotos para ver uma versão
-      ampliada e com uma breve descrição.
-    </p>
-    <ul id="album-fotos">
-      <li id="foto01"><span>- Agenda e lembretes</span></li>
-      <li id="foto02"><span>- Sergey Brin usando o Glass</span></li>
-      <li id="foto03"><span>- Leve e compacto</span></li>
-      <li id="foto04"><span>- Sensação de uma tela de 50"</span></li>
-      <li id="foto05"><span>- Vários tipos de lente</span></li>
-      <li id="foto06"><span>- Informações importantes</span></li>
-    </ul>
-  </article>
+    <article id="main-news">
+        <header id="article-header">
+            <hgroup>
+                <h3>Glass > Fotos</h3>
+                <h1>Galeria de Imagens do Google Glass</h1>
+                <h2>por Gustavo Guanabara</h2>
+                <h3 class="right">Atualizado em 01/Maio/2013</h3>
+            </hgroup>
+        </header>
+        <p>
+            Veja na nossa galeria de fotos várias belas imagens que mostram algumas das principais características do Google Glass, como recursos e propriedades que estão impressionando o mundo inteiro. Basta passar o mouse sobre uma das fotos para ver uma versão
+            ampliada e com uma breve descrição.
+        </p>
+        <ul id="album-fotos">
+            <li id="foto01"><span>- Agenda e lembretes</span></li>
+            <li id="foto02"><span>- Sergey Brin usando o Glass</span></li>
+            <li id="foto03"><span>- Leve e compacto</span></li>
+            <li id="foto04"><span>- Sensação de uma tela de 50"</span></li>
+            <li id="foto05"><span>- Vários tipos de lente</span></li>
+            <li id="foto06"><span>- Informações importantes</span></li>
+        </ul>
+    </article>
 </section>
--- app/views/multimidia.php
+++ app/views/multimidia.php
@@ -1,25 +1,25 @@
 <section id="conteudo">
-  <article id="main-news">
-    <header id="article-header">
-      <hgroup>
-        <h3>Glass > Multimídia</h3>
-        <h1>Sons e Vídeos</h1>
-        <h2>por Gustavo Guanabara</h2>
-        <h3 class="right">Atualizado em 01/Maio/2013</h3>
-      </hgroup>
-    </header>
+    <article id="main-news">
+        <header id="article-header">
+            <hgroup>
+                <h3>Glass > Multimídia</h3>
+                <h1>Sons e Vídeos</h1>
+                <h2>por Gustavo Guanabara</h2>
+                <h3 class="right">Atualizado em 01/Maio/2013</h3>
+            </hgroup>
+        </header>
 
-    <div id="tv-radio">
-      <audio id="musica" controls>
-        <source src="_media/2009-lovers-carvings-bibio.mp3" type="audio/mpeg" />
-        Desculpe, mas não foi possivel carregar o audio.
-      </audio>
-      <video id="filme" controls poster="_imagens/video-mini03.jpg">
-        <source src="_media/getting-started.mp4" type="video/mp4" />
-        Desculpe, mas não foi possivel carregar o vídeo.
-      </video>
-    </div>
-    <iframe id="filme" width="560" height="315" src="https://www.youtube.com/embed/4EvNxWhskf8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
-    </iframe>
-  </article>
+        <div id="tv-radio">
+            <audio id="musica" controls>
+                <source src="_media/2009-lovers-carvings-bibio.mp3" type="audio/mpeg" />
+                Desculpe, mas não foi possivel carregar o audio.
+            </audio>
+            <video id="filme" controls poster="_imagens/video-mini03.jpg">
+                <source src="_media/getting-started.mp4" type="video/mp4" />
+                Desculpe, mas não foi possivel carregar o vídeo.
+            </video>
+        </div>
+        <iframe id="filme" width="560" height="315" src="https://www.youtube.com/embed/4EvNxWhskf8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+        </iframe>
+    </article>
 </section>
